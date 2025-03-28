3d069d9fa660cee427ced6fd8d30b2ebb872610d
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-03-28 20:27:27 +0700

    Hoan thanh kien truc Unet

--- model/UNet.py
+++ model/UNet.py
@@ -10,6 +10,8 @@ class ConvBlock(layers.Layer):
     def __init__(self, filter):
         super(ConvBlock, self).__init__()
 
+        self.dropout = layers.Dropout(0.5)
+
         self.conv1 = layers.Conv2D(
             filters=filter, 
             kernel_size=(3, 3), 
@@ -17,7 +19,8 @@ class ConvBlock(layers.Layer):
             padding="valid", 
             activation="relu",
             kernel_initializer = initializers.HeUniform(),
-            kernel_regularizer = regularizers.L2(0.01)
+            kernel_regularizer = regularizers.L2(0.01),
+            use_bias=False
         )
 
         self.conv2 = layers.Conv2D(
@@ -27,7 +30,8 @@ class ConvBlock(layers.Layer):
             padding="valid", 
             activation="relu",
             kernel_initializer = initializers.HeUniform(),
-            kernel_regularizer = regularizers.L2(0.01)
+            kernel_regularizer = regularizers.L2(0.01),
+            use_bias=False
         )
 
         self.batch_norm = layers.BatchNormalization()
@@ -36,11 +40,20 @@ class ConvBlock(layers.Layer):
     def call(self, x):
         x = self.conv1(x)
         x = self.batch_norm(x)
+        x = self.dropout(x)
 
         x = self.conv2(x)
         x = self.batch_norm(x)
+        x = self.dropout(x)
 
         return x
+
+def copy_crop(down: tf.Tensor, x: tf.Tensor):
+    h = x.shape[1]
+    w = x.shape[2]
+    down = down[:, :w, :h, :]
+
+    return tf.concat([down, x], 3)
 
 class UNET(layers.Layer):
 
@@ -48,9 +61,7 @@ class UNET(layers.Layer):
         super(UNET, self).__init__()
 
         assert feature_fraction in [2**i for i in range(0, 7)]
-        filters = [value / feature_fraction for value in filters]
-        print(filters)
-        print("\n\n\n")
+        filters = [value // feature_fraction for value in filters]
         # [64, 128, 256, 512]
 
         # Encoder
@@ -65,7 +76,15 @@ class UNET(layers.Layer):
         self.bottleneck = ConvBlock(filters[3])
 
         # Decoder
+        self.up_sampling = layers.UpSampling2D((2,2))
 
+        self.up1 = ConvBlock(filters[3])
+        self.up2 = ConvBlock(filters[2])
+        self.up3 = ConvBlock(filters[1])
+        self.up4 = ConvBlock(filters[0])
+
+        # Result
+        self.final = layers.Conv2D(2, (3, 3), (1, 1), "same", kernel_regularizer=regularizers.L2())
 
     def call(self, x):
         # Encoder
@@ -80,13 +99,29 @@ class UNET(layers.Layer):
 
         down4 = self.down4(x)
         x = self.pool(down4)
+        
 
         # Bottleneck
         x = self.bottleneck(x)
 
         # Decoder
-        
-        return x
+        x = self.up_sampling(x)
+        x = copy_crop(down4, x)
+        x = self.up1(x)
+
+        x = self.up_sampling(x)
+        x = copy_crop(down3, x)
+        x = self.up2(x)
+
+        x = self.up_sampling(x)
+        x = copy_crop(down2, x)
+        x = self.up3(x)
+
+        x = self.up_sampling(x)
+        x = copy_crop(down1, x)
+        x = self.up4(x)
+
+        return self.final(x)
     
 if __name__ == "__main__":
     # ConvBlock
@@ -97,5 +132,18 @@ if __name__ == "__main__":
     conv_model = keras.Model(inputs=conv_input, outputs=conv_output)
     conv_result = conv_layer(conv_data)
     print("Conv output shape:", conv_result.shape)
+
+    # Copy crop
+    data1 = tf.random.uniform((1, 64, 64, 512))
+    data2 = tf.random.uniform((1, 56, 56, 512))
+    result = copy_crop(data1, data2)
+    print("Copy and crop shape: ", result.shape)
+
+    # UNET
+    unet_data = tf.random.uniform(shape=(3, 572, 572, 1))
+    unet_input = layers.Input((572, 572, 1), 3)
+    unet_layer = UNET([64, 128, 256, 512])
+    unet_result = unet_layer(unet_data)
+    print("UNET output shape:", unet_result.shape)
 
     
